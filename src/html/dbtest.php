8b383251accb9e2d9dd7b94c100cb96a3394cf7d
F-P-S-B <59065604+F-P-S-B@users.noreply.github.com>
2021-03-10 18:11:29 +0100

    Many changes
    Redesigned the pages

--- src/html/dbtest.php
+++ src/html/dbtest.php
@@ -20,17 +20,28 @@
 
         /** MySQL hostname */
         define('DB_HOST', 'bananef317.mysql.db:3306');
-
+        $ssFamille=1;
         $mysql = new MySQLi(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-        $sql= "SELECT * FROM  ban_famille";
+        $sql= "SELECT inv_nom, inv_unite, inv_prix, inv_qte, inv_description, inv_photo FROM  ban_inventaire WHERE inv_idSousFamille=".$ssFamille;
         $res = $mysql->query($sql);
+        $j=1;
         while (NULL !== ($row = $res->fetch_array())) {
-            for($i=0;$i<sizeof($row);$i++){
-                echo $row[$i];
-                echo '<br/>';
-                echo 'Separateur'.$i;
-                echo '<br/>';
-            }
+            echo "Element ".$j;
+            echo "<br/>";
+            echo "Nom : ".$row[0];
+            echo "<br/>";
+            echo "Unité : ".$row[1];
+            echo "<br/>";
+            echo "Prix : ".$row[2];
+            echo "<br/>";
+            echo "Quantité : ".$row[3];
+            echo "<br/>";
+            echo "Description : ".$row[4];
+            echo "<br/>";
+            echo "Photo : <br/><img src='".$row[5]."' alt='".$row[4]."'/>";
+            echo "<br/>";
+
+            $j++;
         }
         $mysql->close();
     ?>
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,50 @@
-const cellules = document.getElementsByClassName("more-info");
+// Get tables cell
+const lignes = document.getElementsByClassName("more-info");
 
-for (const cellule of cellules) {
-  cellule.addEventListener("click", () => console.log(cellule));
+//Get modal elements for toggling
+const modal = document.getElementById("modal");
+const mdBg = document.getElementById("md-bg");
+const cancelBtn = document.getElementById("cancel");
+
+//Get modal elements for content
+const input_qtt = document.getElementById("input-qtt");
+const input_prix = document.getElementById("input-prix");
+
+//Add modal activation
+mdBg.addEventListener("click", () => {
+  document.body.classList.remove("main");
+  modal.classList.remove("active");
+  input_prix.value = "";
+  input_qtt.value = "";
+});
+cancelBtn.addEventListener("click", () => {
+  document.body.classList.remove("main");
+  modal.classList.remove("active");
+});
+
+//Add modal content
+input_qtt.addEventListener("change", () => {
+  input_prix.value =
+    Math.round(
+      input_qtt.value.replace(",", ".").replace(" ", "") * 100 * prix
+    ) / 100;
+});
+input_prix.addEventListener("change", () => {
+  input_qtt.value =
+    Math.round((input_prix.value.replace(",", ".") * 100) / prix) / 100;
+});
+
+for (let i = 0; i < lignes.length; i++) {
+  const ligne = lignes[i];
+  ligne.addEventListener("click", () => {
+    document.body.classList.add("main");
+    modal.classList.add("active");
+    cellules = ligne.getElementsByTagName("td");
+    modal.getElementsByTagName("header")[0].innerHTML = cellules[0].innerText;
+    document.getElementById("prix").innerText =
+      cellules[2].innerText + " / " + cellules[3].innerText;
+    prix = parseInt(cellules[2].innerText.replace("€", ""));
+    document.getElementById("description").innerText =
+      cellules[0].dataset.description;
+  });
 }
